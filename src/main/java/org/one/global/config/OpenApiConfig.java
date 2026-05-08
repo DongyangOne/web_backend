39ccbafcf225f2920762a4b68153a6ba8fcc40ca
Chunwol <60349340+Chunwol@users.noreply.github.com>
2026-05-08 19:37:30 +0900

    feat: Swagger/OpenAPI - Bearer 인증 스키마 + API 그룹핑

--- src/main/java/org/one/global/config/OpenApiConfig.java
+++ src/main/java/org/one/global/config/OpenApiConfig.java
@@ -1,14 +1,20 @@
 package org.one.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+	private static final String BEARER_SCHEME = "bearerAuth";
 
 	@Bean
 	public OpenAPI openAPI() {
@@ -18,6 +24,28 @@ public class OpenApiConfig {
 						.description("ONE 웹사이트 백엔드 API 문서")
 						.version("1.0.0")
 						.contact(new Contact().name("ONE Team").email("contact@one.dev"))
-						.license(new License().name("MIT").url("https://opensource.org/licenses/MIT")));
+						.license(new License().name("MIT").url("https://opensource.org/licenses/MIT")))
+					.components(new Components()
+							.addSecuritySchemes(BEARER_SCHEME, new SecurityScheme()
+									.type(SecurityScheme.Type.HTTP)
+									.scheme("bearer")
+									.bearerFormat("JWT")))
+						.addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME));
+	}
+
+	@Bean
+	public GroupedOpenApi authApi() {
+		return GroupedOpenApi.builder()
+				.group("auth")
+				.pathsToMatch("/auth/**")
+				.build();
+	}
+
+	@Bean
+	public GroupedOpenApi fileApi() {
+		return GroupedOpenApi.builder()
+				.group("files")
+				.pathsToMatch("/api/files/**")
+				.build();
 	}
 }
