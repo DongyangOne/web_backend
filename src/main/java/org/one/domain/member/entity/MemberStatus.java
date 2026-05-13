1092fcfd44906c3729b1d1f59a83d0a990708344
Chunwol <60349340+Chunwol@users.noreply.github.com>
2026-05-13 09:59:58 +0900

    refactor: 기존 도메인/공통 코드 위치 정리 (파일 삭제/이전)

--- src/main/java/org/one/domain/member/entity/MemberStatus.java
+++
@@ -1,9 +0,0 @@
-package org.one.domain.member.entity;
-
-public enum MemberStatus {
-    ACTIVE,          // 재학
-    MILITARY_LEAVE,  // 군휴학
-    GRADUATED,       // 졸업
-    WITHDRAWN,       // 탈퇴
-    ON_LEAVE         // 휴학
-}
