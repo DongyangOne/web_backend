480a65d567ce361051b61b856978c09327939451
Chunwol <60349340+Chunwol@users.noreply.github.com>
2026-05-16 19:27:17 +0900

    feat: 관리자 프로젝트 CRUD 및 사진 개별 추가/삭제/순서 변경 API 구현

--- src/main/java/org/one/domain/entity/ProjectPhoto.java
+++ src/main/java/org/one/domain/entity/ProjectPhoto.java
@@ -78,4 +78,12 @@ public class ProjectPhoto extends BaseEntity {
 	 */
 	public Integer getPriority() { return priority; }
 
+	/**
+	 * 사진 노출 순서를 수정합니다.
+	 *
+	 * @param priority 새 노출 순서
+	 */
+	public void updatePriority(Integer priority) {
+		this.priority = priority;
+	}
 }
