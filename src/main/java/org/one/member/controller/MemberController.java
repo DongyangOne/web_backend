d4dbb3910da4dbd5c170dbfbd41bf4858f1943db
marianddi <hanna4331@naver.com>
2026-05-31 16:13:03 +0900

    docs : ApiErrorEceptions어노테이션에 에러 추가

--- src/main/java/org/one/member/controller/MemberController.java
+++ src/main/java/org/one/member/controller/MemberController.java
@@ -63,7 +63,7 @@ public class MemberController {
      *
      * 응답 데이터 : 성공 메세지
      */
-    @ApiErrorExceptions({ErrorCode.DUPLICATE_PHONE_NUMBER, ErrorCode.DUPLICATE_STUDENT_ID})
+    @ApiErrorExceptions({ErrorCode.DUPLICATE_PHONE_NUMBER, ErrorCode.DUPLICATE_STUDENT_ID, ErrorCode.INVALID_INPUT})
     @Operation(summary = "부원 등록", description = "관리자 권한(ADMIN)이 있는 계정만 전체 부원 명부를 조회할 수 있습니다.")
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
